0508462e95bff92ae5f7431d9bba53a83c1d513f
Dev Bambino <devbambinoacc@gmail.com>
2023-10-18 16:40:11 -0600

    starting working on superfundrs contract

--- contracts/SuperFundrs.sol
+++ contracts/SuperFundrs.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+contract Organization{
+    bytes32 private immutable id;//organization web domain
+    address private immutable admin;//organization admin
+
+    constructor(bytes32 _orgId,address _admin){
+        id = _orgId;
+        admin = _admin;
+    }
+
+}
+
 contract SuperFundrs{
-	
+    mapping(address => Organization[]) private users;//user's address -> orgs' addresses
 }
